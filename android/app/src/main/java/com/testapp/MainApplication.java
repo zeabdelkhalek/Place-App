71a0c7178646981911a6f3a2d842870c7f9d055b
Abdelkhalek <ha_zellat@esi.dz>
2018-10-18 05:03:25 +0100

    add google maps

--- android/app/src/main/java/com/testapp/MainApplication.java
+++ android/app/src/main/java/com/testapp/MainApplication.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
  import com.reactnativenavigation.NavigationApplication;
-
+import com.airbnb.android.react.maps.MapsPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -55,7 +55,8 @@ public class MainApplication extends NavigationApplication   {
          // Add additional packages you require here
          // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new MapsPackage()
          );
      }
 
--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import { View, Image, Button, StyleSheet, Text } from "react-native";
-
+import { View, Image, Button, StyleSheet, Text , Dimensions } from "react-native";
+import MapView from 'react-native-maps';
 class PickLocation extends Component {
+
+  state = { 
+    focusedLocation : {
+      latitude : 37.7900352,
+      longitude : -122.4013726, 
+      latitudeDelta: 0.0122,
+      longitudeDelta : Dimensions.get("window").width / Dimensions.get("window").height * 0.0122 , 
+
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+        initialRegion={this.state.focusedLocation}
+        style={styles.map}
+        />
         <View style={styles.button}>
           <Button title="Locate Me" onPress={() => alert('Pick Location!')} />
         </View>
@@ -30,6 +41,10 @@ const styles = StyleSheet.create({
   },
   button: {
     margin: 8
+  }, 
+  map : { 
+    width : "100%" , 
+    height : 250
   }
 });
 
